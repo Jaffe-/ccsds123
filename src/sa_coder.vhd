cbda01e7da03f170daffc38822a0825e26504090
Johan Fjeldtvedt <jaffe1@gmail.com>
2018-02-02 11:28:11 +0100

    add accumulator RAM to encoder

--- src/sa_coder.vhd
+++ src/sa_coder.vhd
@@ -8,6 +8,7 @@ entity sa_encoder is
     NZ            : integer := 500;
     D             : integer := 12;
     UMAX          : integer := 9;
+    KZ_PRIME      : integer := 8;
     COUNTER_SIZE  : integer := 8;
     INITIAL_COUNT : integer := 6
     );
@@ -21,19 +22,16 @@ entity sa_encoder is
     in_z        : in integer range 0 to NZ-1;
     in_residual : in unsigned(D-1 downto 0);
 
-    accumulator_rd_z    : out integer range 0 to NZ-1;
-    accumulator_rd_data : in  integer range 0 to 2**(D+COUNTER_SIZE)-1;
-    accumulator_wr_z    : out integer range 0 to NZ-1;
-    accumulator_wr_data : out integer range 0 to 2**(D+COUNTER_SIZE)-1;
-
     out_valid    : out std_logic;
+    out_last     : out std_logic;
     out_data     : out std_logic_vector(UMAX + D-1 downto 0);
     out_num_bits : out integer range 0 to UMAX + D
     );
 end sa_encoder;
 
 architecture rtl of sa_encoder is
-  signal counter : integer range 0 to 2**COUNTER_SIZE-1;
+  signal counter     : integer range 0 to 2**COUNTER_SIZE-1;
+  signal counter_reg : integer range 0 to 2**COUNTER_SIZE-1;
 
   type z_arr_t is array (0 to 1) of integer range 0 to NZ-1;
   signal z_regs : z_arr_t;
@@ -52,22 +50,55 @@ architecture rtl of sa_encoder is
   signal k_z           : integer range 0 to D-2;
   signal code_word     : std_logic_vector(UMAX + D - 1 downto 0);
   signal code_num_bits : integer range 0 to UMAX + D;
+
+  subtype accumulator_t is integer range 0 to 2**(D+COUNTER_SIZE)-1;
+  signal accumulator_rd_data : accumulator_t;
+  signal accumulator_wr      : std_logic;
+  signal accumulator_wr_z    : integer range 0 to NZ-1;
+  signal accumulator_wr_data : accumulator_t;
+
+  type accumulator_arr_t is array (0 to NZ-1) of accumulator_t;
+  signal accumulators : accumulator_arr_t;
 begin
+
+  -- RAM write port
+  process (clk)
+  begin
+    if (rising_edge(clk)) then
+      if (accumulator_wr = '1') then
+        accumulators(accumulator_wr_z) <= accumulator_wr_data;
+      end if;
+    end if;
+  end process;
+
+  -- RAM read port
+  process (clk)
+  begin
+    if (rising_edge(clk)) then
+      if (in_valid = '1') then
+        accumulator_rd_data <= accumulators(in_z);
+      end if;
+    end if;
+  end process;
+
   -- Counter and accumulator update
   process (clk)
   begin
     if (rising_edge(clk)) then
       if (aresetn = '0') then
-        counter             <= 0;
-        accumulator_wr_data <= 0;
+        counter <= 0;
       else
         if (in_valid = '1') then
-          if (counter < 2**COUNTER_SIZE-1) then
-            counter             <= counter + 1;
-            accumulator_wr_data <= accumulator_rd_data + to_integer(in_residual);
+          if (in_ctrl.first_in_line = '1' and in_ctrl.first_line = '1') then
+            counter <= 2**INITIAL_COUNT;
           else
-            counter             <= (counter + 1)/2;
-            accumulator_wr_data <= (accumulator_rd_data + to_integer(in_residual) + 1)/2;
+            if (in_z = NZ-1) then
+              if (counter < 2**COUNTER_SIZE-1) then
+                counter <= counter + 1;
+              else
+                counter <= (counter + 1)/2;
+              end if;
+            end if;
           end if;
         end if;
       end if;
@@ -80,18 +111,24 @@ begin
   begin
     if (rising_edge(clk)) then
       if (aresetn = '0') then
-        counter       <= 2**INITIAL_COUNT;
-        rhs           <= 0;
-        rhs_part      <= 0;
-        z_regs        <= (others => 0);
-        ctrl_regs     <= (others => ('0', '0', '0', '0'));
-        residual_regs <= (others => (others => '0'));
-        valid_regs    <= (others => '0');
+        counter             <= 2**INITIAL_COUNT;
+        rhs                 <= 0;
+        rhs_part            <= 0;
+        z_regs              <= (others => 0);
+        ctrl_regs           <= (others => ('0', '0', '0', '0'));
+        residual_regs       <= (others => (others => '0'));
+        valid_regs          <= (others => '0');
+        accumulator_wr      <= '0';
+        accumulator_wr_data <= 0;
+        accumulator_wr_z    <= 0;
       else
         --------------------------------------------------------------------------------
         -- Stage 1 - Compute floor(49/2^7 * counter(t))
+        -- Accumulator for current band is fetched from RAM during this cycle
         --------------------------------------------------------------------------------
-        rhs_part         <= (49 * counter) / 2**7;
+        rhs_part    <= (49 * counter) / 2**7;
+        counter_reg <= counter;
+
         valid_regs(0)    <= in_valid;
         z_regs(0)        <= in_z;
         ctrl_regs(0)     <= in_ctrl;
@@ -99,9 +136,22 @@ begin
 
         --------------------------------------------------------------------------------
         -- Stage 2 - Compute right hand side in the inequalities for
-        -- determining k_z(t)
+        -- determining k_z(t), compute next accumulator
         --------------------------------------------------------------------------------
-        rhs              <= accumulator_rd_data + rhs_part;
+        rhs <= accumulator_rd_data + rhs_part;
+
+        if (ctrl_regs(0).first_in_line = '1' and ctrl_regs(0).first_line = '1') then
+          accumulator_wr_data <= ((3 * 2**(KZ_PRIME + 6) - 49) * 2**INITIAL_COUNT) / 2**7;
+        else
+          if (counter_reg < 2**COUNTER_SIZE - 1) then
+            accumulator_wr_data <= accumulator_rd_data + to_integer(in_residual);
+          else
+            accumulator_wr_data <= (accumulator_rd_data + to_integer(in_residual) + 1) / 2;
+          end if;
+        end if;
+        accumulator_wr   <= valid_regs(0);
+        accumulator_wr_z <= z_regs(0);
+
         valid_regs(1)    <= valid_regs(0);
         z_regs(1)        <= z_regs(0);
         ctrl_regs(1)     <= ctrl_regs(0);
@@ -147,13 +197,13 @@ begin
         end if;
         valid_regs(3) <= valid_regs(2);
         ctrl_regs(3)  <= ctrl_regs(2);
+
       end if;
     end if;
   end process;
 
-  accumulator_rd_z <= in_z;
-  out_valid        <= valid_regs(3);
-  out_last         <= ctrl_regs(3).last;
-  out_data         <= code_word;
-  out_num_bits     <= code_num_bits;
+  out_valid    <= valid_regs(3);
+  out_last     <= ctrl_regs(3).last;
+  out_data     <= code_word;
+  out_num_bits <= code_num_bits;
 end rtl;
