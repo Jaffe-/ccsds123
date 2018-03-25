90443c64472aef9205748220d70baac73c69231f
Johan Fjeldtvedt <jaffe1@gmail.com>
2018-03-25 21:10:28 +0200

    add FIFO to combiner

--- src/combiner_pre.vhd
+++ src/combiner_pre.vhd
@@ -7,11 +7,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.all;
 use ieee.math_real.all;
 
+library xpm;
+use xpm.vcomponents.all;
+
 entity combiner is
   generic (
-    BLOCK_SIZE : integer := 32;
+    BLOCK_SIZE : integer := 64;
     N_WORDS    : integer := 4;
-    MAX_LENGTH : integer := 48
+    MAX_LENGTH : integer := 30
     );
   port (
     clk     : in std_logic;
@@ -47,12 +50,16 @@ architecture rtl of combiner is
   signal valid_regs   : std_logic_vector(1 downto 0);
   signal last_regs    : std_logic_vector(1 downto 0);
 
-  function limit(x : integer; LIM : integer) return integer is
-    variable v : integer range 0 to LIM;
-  begin
-    v := x;
-    return x;
-  end limit;
+  constant FIFO_SIZE       : integer := N_WORDS*(LENGTH_BITS + MAX_LENGTH) + 1;
+  signal fifo_rden         : std_logic;
+  signal fifo_empty        : std_logic;
+  signal fifo_in           : std_logic_vector(FIFO_SIZE-1 downto 0);
+  signal fifo_out          : std_logic_vector(FIFO_SIZE-1 downto 0);
+  signal from_fifo_words   : std_logic_vector(in_words'range);
+  signal from_fifo_lengths : unsigned(in_lengths'range);
+  signal from_fifo_last    : std_logic;
+  signal from_fifo_valid   : std_logic;
+
 begin
 
   process (full_blocks)
@@ -61,6 +68,56 @@ begin
       out_words((i+1)*BLOCK_SIZE-1 downto i*BLOCK_SIZE) <= full_blocks(i);
     end loop;
   end process;
+
+  fifo_in <= in_words & std_logic_vector(in_lengths) & in_last;
+
+  i_fifo : xpm_fifo_sync
+    generic map (
+
+      FIFO_MEMORY_TYPE    => "auto",  --string; "auto", "block", "distributed", or "ultra" ;
+      ECC_MODE            => "no_ecc",  --string; "no_ecc" or "en_ecc";
+      FIFO_WRITE_DEPTH    => 2048,      --positive integer
+      WRITE_DATA_WIDTH    => FIFO_SIZE,  --positive integer
+      WR_DATA_COUNT_WIDTH => 12,        --positive integer
+      PROG_FULL_THRESH    => 10,        --positive integer
+      FULL_RESET_VALUE    => 0,         --positive integer; 0 or 1;
+      READ_MODE           => "std",     --string; "std" or "fwft";
+      FIFO_READ_LATENCY   => 1,         --positive integer;
+      READ_DATA_WIDTH     => FIFO_SIZE,  --positive integer
+      RD_DATA_COUNT_WIDTH => 12,        --positive integer
+      PROG_EMPTY_THRESH   => 10,        --positive integer
+      DOUT_RESET_VALUE    => "0",       --string
+      WAKEUP_TIME         => 0          --positive integer; 0 or 2;
+      )
+    port map (
+      rst           => not aresetn,
+      wr_clk        => clk,
+      wr_en         => in_valid,
+      din           => fifo_in,
+      full          => open,
+      overflow      => open,
+      wr_rst_busy   => open,
+      rd_en         => fifo_rden,
+      dout          => fifo_out,
+      empty         => fifo_empty,
+      underflow     => open,
+      rd_rst_busy   => open,
+      prog_full     => open,
+      wr_data_count => open,
+      prog_empty    => open,
+      rd_data_count => open,
+      sleep         => '0',
+      injectsbiterr => '0',
+      injectdbiterr => '0',
+      sbiterr       => open,
+      dbiterr       => open
+      );
+
+  from_fifo_words   <= fifo_out(fifo_out'high downto fifo_out'high-MAX_LENGTH*N_WORDS + 1);
+  from_fifo_lengths <= unsigned(fifo_out(fifo_out'high-MAX_LENGTH*N_WORDS downto 1));
+  from_fifo_last    <= fifo_out(0);
+
+  fifo_rden <= not fifo_empty;
 
   process (clk)
     constant SUM_SIZE           : integer := integer(ceil(log2(real(BLOCK_SIZE + MAX_LENGTH))));
@@ -78,22 +135,28 @@ begin
         num_remaining_bits := (others => '0');
         valid_regs         <= (others => '0');
       else
+        if (fifo_rden = '1') then
+          from_fifo_valid <= '1';
+        else
+          from_fifo_valid <= '0';
+        end if;
+
         --------------------------------------------------------------------------------
         -- Stage 1 - Compute shift amounts and number of blocks
         --------------------------------------------------------------------------------
         if (in_valid = '1') then
           for i in 0 to N_WORDS-1 loop
-            words(0, i) <= in_words((i+1)*MAX_LENGTH-1 downto i*MAX_LENGTH) & (BLOCK_SIZE-2 downto 0 => '0');
+            words(0, i) <= from_fifo_words((i+1)*MAX_LENGTH-1 downto i*MAX_LENGTH) & (BLOCK_SIZE-2 downto 0 => '0');
 
             -- Compute bits left after this word is shifted in
-            sum                := resize(num_remaining_bits, SUM_SIZE) + in_lengths((i+1)*LENGTH_BITS-1 downto i*LENGTH_BITS);
+            sum                := resize(num_remaining_bits, SUM_SIZE) + from_fifo_lengths((i+1)*LENGTH_BITS-1 downto i*LENGTH_BITS);
             shift_arr(i)       <= to_integer(num_remaining_bits);
             n_blocks_arr(0, i) <= to_integer(sum(sum'high downto BLOCK_SIZE_BITS));
             num_remaining_bits := sum(BLOCK_SIZE_BITS-1 downto 0);
           end loop;
         end if;
-        valid_regs(0) <= in_valid;
-        last_regs(0)  <= in_last;
+        valid_regs(0) <= from_fifo_valid;
+        last_regs(0)  <= from_fifo_last;
 
         --------------------------------------------------------------------------------
         -- Stage 2 - Shift each incoming word
@@ -125,10 +188,10 @@ begin
             remaining_bits := temp;
           end loop;
 
-          --if (last_regs(1) = '1') then
-          --  full_blocks(count) <= remaining_bits(BLOCK_SIZE+MAX_LENGTH-2 downto MAX_LENGTH-1);
-          --  count := count + 1;
-          --end if;
+          if (last_regs(1) = '1') then
+            full_blocks(count) <= remaining_bits(BLOCK_SIZE+MAX_LENGTH-2 downto MAX_LENGTH-1);
+            count              := count + 1;
+          end if;
         end if;
         out_count <= count;
         out_valid <= valid_regs(1);
