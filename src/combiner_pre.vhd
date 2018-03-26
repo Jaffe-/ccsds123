bd1b6fb2cbf624703fd02c92abfafa74cca2dd44
Johan Fjeldvedt <jaffe1@gmail.com>
2018-03-26 16:13:58 +0200

    add 64-bit output to combiner

--- src/combiner_pre.vhd
+++ src/combiner_pre.vhd
@@ -14,7 +14,7 @@ entity combiner is
   generic (
     BLOCK_SIZE : integer := 64;
     N_WORDS    : integer := 4;
-    MAX_LENGTH : integer := 30
+    MAX_LENGTH : integer := 48
     );
   port (
     clk     : in std_logic;
@@ -25,8 +25,7 @@ entity combiner is
     in_valid   : in std_logic;
     in_last    : in std_logic;
 
-    out_words : out std_logic_vector(((BLOCK_SIZE + N_WORDS * MAX_LENGTH)/BLOCK_SIZE + 1)*BLOCK_SIZE - 1 downto 0);
-    out_count : out integer range 0 to (BLOCK_SIZE + N_WORDS * MAX_LENGTH) / BLOCK_SIZE + 1;
+    out_data  : out std_logic_vector(BLOCK_SIZE-1 downto 0);
     out_valid : out std_logic;
     out_last  : out std_logic
     );
@@ -50,74 +49,23 @@ architecture rtl of combiner is
   signal valid_regs   : std_logic_vector(1 downto 0);
   signal last_regs    : std_logic_vector(1 downto 0);
 
-  constant FIFO_SIZE       : integer := N_WORDS*(LENGTH_BITS + MAX_LENGTH) + 1;
-  signal fifo_rden         : std_logic;
-  signal fifo_empty        : std_logic;
-  signal fifo_in           : std_logic_vector(FIFO_SIZE-1 downto 0);
-  signal fifo_out          : std_logic_vector(FIFO_SIZE-1 downto 0);
-  signal from_fifo_words   : std_logic_vector(in_words'range);
-  signal from_fifo_lengths : unsigned(in_lengths'range);
-  signal from_fifo_last    : std_logic;
-  signal from_fifo_valid   : std_logic;
+  constant FIFO_SIZE : integer := MAX_BLOCKS * BLOCK_SIZE + 5;
 
+  signal fifo_rden        : std_logic;
+  signal fifo_wren        : std_logic;
+  signal fifo_empty       : std_logic;
+  signal fifo_in          : std_logic_vector(FIFO_SIZE-1 downto 0);
+  signal fifo_out         : std_logic_vector(FIFO_SIZE-1 downto 0);
+  signal to_fifo_count    : integer range 0 to MAX_BLOCKS;
+  signal to_fifo_last     : std_logic;
+  signal from_fifo_last   : std_logic;
+  signal from_fifo_valid  : std_logic;
+  signal from_fifo_count  : integer range 0 to MAX_BLOCKS;
+  signal from_fifo_blocks : full_blocks_t;
+
+  signal counter    : integer range 0 to MAX_BLOCKS;
 begin
 
-  process (full_blocks)
-  begin
-    for i in full_blocks'range loop
-      out_words((i+1)*BLOCK_SIZE-1 downto i*BLOCK_SIZE) <= full_blocks(i);
-    end loop;
-  end process;
-
-  fifo_in <= in_words & std_logic_vector(in_lengths) & in_last;
-
-  i_fifo : xpm_fifo_sync
-    generic map (
-
-      FIFO_MEMORY_TYPE    => "auto",  --string; "auto", "block", "distributed", or "ultra" ;
-      ECC_MODE            => "no_ecc",  --string; "no_ecc" or "en_ecc";
-      FIFO_WRITE_DEPTH    => 2048,      --positive integer
-      WRITE_DATA_WIDTH    => FIFO_SIZE,  --positive integer
-      WR_DATA_COUNT_WIDTH => 12,        --positive integer
-      PROG_FULL_THRESH    => 10,        --positive integer
-      FULL_RESET_VALUE    => 0,         --positive integer; 0 or 1;
-      READ_MODE           => "std",     --string; "std" or "fwft";
-      FIFO_READ_LATENCY   => 1,         --positive integer;
-      READ_DATA_WIDTH     => FIFO_SIZE,  --positive integer
-      RD_DATA_COUNT_WIDTH => 12,        --positive integer
-      PROG_EMPTY_THRESH   => 10,        --positive integer
-      DOUT_RESET_VALUE    => "0",       --string
-      WAKEUP_TIME         => 0          --positive integer; 0 or 2;
-      )
-    port map (
-      rst           => not aresetn,
-      wr_clk        => clk,
-      wr_en         => in_valid,
-      din           => fifo_in,
-      full          => open,
-      overflow      => open,
-      wr_rst_busy   => open,
-      rd_en         => fifo_rden,
-      dout          => fifo_out,
-      empty         => fifo_empty,
-      underflow     => open,
-      rd_rst_busy   => open,
-      prog_full     => open,
-      wr_data_count => open,
-      prog_empty    => open,
-      rd_data_count => open,
-      sleep         => '0',
-      injectsbiterr => '0',
-      injectdbiterr => '0',
-      sbiterr       => open,
-      dbiterr       => open
-      );
-
-  from_fifo_words   <= fifo_out(fifo_out'high downto fifo_out'high-MAX_LENGTH*N_WORDS + 1);
-  from_fifo_lengths <= unsigned(fifo_out(fifo_out'high-MAX_LENGTH*N_WORDS downto 1));
-  from_fifo_last    <= fifo_out(0);
-
-  fifo_rden <= not fifo_empty;
 
   process (clk)
     constant SUM_SIZE           : integer := integer(ceil(log2(real(BLOCK_SIZE + MAX_LENGTH))));
@@ -135,28 +83,22 @@ begin
         num_remaining_bits := (others => '0');
         valid_regs         <= (others => '0');
       else
-        if (fifo_rden = '1') then
-          from_fifo_valid <= '1';
-        else
-          from_fifo_valid <= '0';
-        end if;
-
         --------------------------------------------------------------------------------
         -- Stage 1 - Compute shift amounts and number of blocks
         --------------------------------------------------------------------------------
         if (in_valid = '1') then
           for i in 0 to N_WORDS-1 loop
-            words(0, i) <= from_fifo_words((i+1)*MAX_LENGTH-1 downto i*MAX_LENGTH) & (BLOCK_SIZE-2 downto 0 => '0');
+            words(0, i) <= in_words((i+1)*MAX_LENGTH-1 downto i*MAX_LENGTH) & (BLOCK_SIZE-2 downto 0 => '0');
 
             -- Compute bits left after this word is shifted in
-            sum                := resize(num_remaining_bits, SUM_SIZE) + from_fifo_lengths((i+1)*LENGTH_BITS-1 downto i*LENGTH_BITS);
+            sum                := resize(num_remaining_bits, SUM_SIZE) + in_lengths((i+1)*LENGTH_BITS-1 downto i*LENGTH_BITS);
             shift_arr(i)       <= to_integer(num_remaining_bits);
             n_blocks_arr(0, i) <= to_integer(sum(sum'high downto BLOCK_SIZE_BITS));
             num_remaining_bits := sum(BLOCK_SIZE_BITS-1 downto 0);
           end loop;
         end if;
-        valid_regs(0) <= from_fifo_valid;
-        last_regs(0)  <= from_fifo_last;
+        valid_regs(0) <= in_valid;
+        last_regs(0)  <= in_last;
 
         --------------------------------------------------------------------------------
         -- Stage 2 - Shift each incoming word
@@ -193,11 +135,108 @@ begin
             count              := count + 1;
           end if;
         end if;
-        out_count <= count;
-        out_valid <= valid_regs(1);
-        out_last  <= last_regs(1);
+        to_fifo_count <= count;
+        if (valid_regs(1) = '1' and count /= 0) then
+          fifo_wren <= '1';
+        else
+          fifo_wren <= '0';
+        end if;
+        to_fifo_last <= last_regs(1);
       end if;
     end if;
   end process;
 
+  process (full_blocks, to_fifo_count, to_fifo_last)
+  begin
+    for i in full_blocks'range loop
+      fifo_in((i+1)*BLOCK_SIZE-1 downto i*BLOCK_SIZE) <= full_blocks(i);
+    end loop;
+    fifo_in(MAX_BLOCKS*BLOCK_SIZE+3 downto MAX_BLOCKS*BLOCK_SIZE) <= std_logic_vector(to_unsigned(to_fifo_count, 4));
+    fifo_in(fifo_in'high)                                         <= to_fifo_last;
+  end process;
+
+  i_fifo : xpm_fifo_sync
+    generic map (
+
+      FIFO_MEMORY_TYPE    => "auto",  --string; "auto", "block", "distributed", or "ultra" ;
+      ECC_MODE            => "no_ecc",  --string; "no_ecc" or "en_ecc";
+      FIFO_WRITE_DEPTH    => 256,       --positive integer
+      WRITE_DATA_WIDTH    => FIFO_SIZE,  --positive integer
+      WR_DATA_COUNT_WIDTH => 9,         --positive integer
+      PROG_FULL_THRESH    => 10,        --positive integer
+      FULL_RESET_VALUE    => 0,         --positive integer; 0 or 1;
+      READ_MODE           => "std",     --string; "std" or "fwft";
+      FIFO_READ_LATENCY   => 1,         --positive integer;
+      READ_DATA_WIDTH     => FIFO_SIZE,  --positive integer
+      RD_DATA_COUNT_WIDTH => 9,         --positive integer
+      PROG_EMPTY_THRESH   => 10,        --positive integer
+      DOUT_RESET_VALUE    => "0",       --string
+      WAKEUP_TIME         => 0          --positive integer; 0 or 2;
+      )
+    port map (
+      rst           => not aresetn,
+      wr_clk        => clk,
+      wr_en         => fifo_wren,
+      din           => fifo_in,
+      full          => open,
+      overflow      => open,
+      wr_rst_busy   => open,
+      rd_en         => fifo_rden,
+      dout          => fifo_out,
+      empty         => fifo_empty,
+      underflow     => open,
+      rd_rst_busy   => open,
+      prog_full     => open,
+      wr_data_count => open,
+      prog_empty    => open,
+      rd_data_count => open,
+      sleep         => '0',
+      injectsbiterr => '0',
+      injectdbiterr => '0',
+      sbiterr       => open,
+      dbiterr       => open
+      );
+
+  fifo_rden       <= '1' when fifo_empty = '0' and (from_fifo_valid = '0' or counter = from_fifo_count - 1) else '0';
+  from_fifo_count <= to_integer(unsigned(fifo_out(fifo_out'high - 1 downto fifo_out'high-4)));
+  from_fifo_last  <= fifo_out(fifo_out'high);
+  process (fifo_out)
+  begin
+    for i in from_fifo_blocks'range loop
+      from_fifo_blocks(i) <= fifo_out((i+1)*BLOCK_SIZE-1 downto i*BLOCK_SIZE);
+    end loop;
+  end process;
+
+  process (clk)
+  begin
+    if (rising_edge(clk)) then
+      if (aresetn = '0') then
+        counter         <= 0;
+        from_fifo_valid <= '0';
+      else
+        if (fifo_rden = '1') then
+          from_fifo_valid <= '1';
+        elsif (counter = from_fifo_count - 1) then
+          from_fifo_valid <= '0';
+        end if;
+
+        if (fifo_rden = '1') then
+          counter <= 0;
+        elsif (from_fifo_valid = '1') then
+          counter <= counter + 1;
+        end if;
+      end if;
+    end if;
+  end process;
+
+  out_data  <= from_fifo_blocks(counter);
+  out_valid <= from_fifo_valid;
+  process (counter, from_fifo_last, from_fifo_count)
+  begin
+    if (from_fifo_last = '1' and counter = from_fifo_count - 1) then
+      out_last <= '1';
+    else
+      out_last <= '0';
+    end if;
+  end process;
 end rtl;
