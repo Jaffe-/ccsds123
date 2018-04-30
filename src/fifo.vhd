83160372bb2999dd69f0055dd5e06594e351ce2c
Johan Fjeldtvedt <jaffe1@gmail.com>
2018-04-30 21:06:24 +0200

    fifo.vhd: use block ram wrapper

--- src/fifo.vhd
+++ src/fifo.vhd
@@ -6,7 +6,8 @@ use work.common.all;
 entity fifo is
   generic (
     ELEMENT_SIZE : integer;
-    SIZE         : integer
+    SIZE         : integer;
+    RAM_TYPE     : string := "block"
     );
   port (
     clk     : in std_logic;
@@ -20,40 +21,34 @@ entity fifo is
 end fifo;
 
 architecture rtl of fifo is
-  type fifo_t is array(0 to SIZE-1) of std_logic_vector(ELEMENT_SIZE-1 downto 0);
-
-  signal fifo   : fifo_t;
-  signal rd_idx : integer range 0 to fifo'high;
-  signal wr_idx : integer range 0 to fifo'high;
+  signal rd_idx : integer range 0 to SIZE-1;
+  signal wr_idx : integer range 0 to SIZE-1;
 begin
-  process (clk)
-  begin
-    if (rising_edge(clk)) then
-      if (in_valid = '1') then
-        fifo(wr_idx) <= in_data;
-      end if;
-    end if;
-  end process;
-
-  process (clk)
-  begin
-    if (rising_edge(clk)) then
-      if (in_valid = '1') then
-        out_data <= fifo(rd_idx);
-      end if;
-    end if;
-  end process;
+  i_dp_bram : entity work.dp_bram
+    generic map (
+      ELEMENTS     => SIZE,
+      ELEMENT_SIZE => ELEMENT_SIZE,
+      RAM_TYPE     => RAM_TYPE)
+    port map (
+      clk     => clk,
+      aresetn => aresetn,
+      wr      => in_valid,
+      wraddr  => wr_idx,
+      wrdata  => in_data,
+      rd      => in_valid,
+      rdaddr  => rd_idx,
+      rddata  => out_data);
 
   process (clk)
   begin
     if (rising_edge(clk)) then
       if (aresetn = '0') then
         rd_idx <= 0;
-        wr_idx <= fifo'high;
+        wr_idx <= SIZE-1;
       else
         if (in_valid = '1') then
-          rd_idx <= wrap_inc(rd_idx, fifo'high);
-          wr_idx <= wrap_inc(wr_idx, fifo'high);
+          rd_idx <= wrap_inc(rd_idx, SIZE-1);
+          wr_idx <= wrap_inc(wr_idx, SIZE-1);
         end if;
       end if;
     end if;
